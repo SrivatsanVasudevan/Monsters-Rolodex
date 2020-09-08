fd884e8a962ec9037659ddee1714ad94848ee5d5
SrivatsanVasudevan <srivats1@uw.edu>
2020-09-08 15:13:06 -0700

    Updates

---
+++ precache-manifest.200b237a3e4707a3e6ab472d09393447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8213b9bc5d363609870897a915b6762c",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "740f263f369d4b8a75a4",
+    "url": "/Monsters-Rolodex/static/css/main.4a8e48ce.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740f263f369d4b8a75a4",
+    "url": "/Monsters-Rolodex/static/js/main.64424bd3.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  }
+]);
